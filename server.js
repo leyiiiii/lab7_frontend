adb8b87feed5983bd4deb1680a294325839e6f55
70216002黄乐怡 <mikiwongly@hotmail.my>
2022-05-07 19:02:26 +0800

    1

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get(/.*/, function(req,res){
     res.sendFile(path.join(_dirname,'/dist/index.html'))
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
