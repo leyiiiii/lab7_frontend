6acc45dd25cd0b62f21948b58d887eb89425b873
70216002黄乐怡 <mikiwongly@hotmail.my>
2022-05-07 19:37:33 +0800

    last

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <img src="../assets/test.jpeg" alt="img" :style="{width:'100px'}">
+    <h1>70216002 黄乐怡</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
