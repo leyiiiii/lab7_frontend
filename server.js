6e249ad69e3941beaa3c3794f325f0d686923e2d
70216002黄乐怡 <mikiwongly@hotmail.my>
2022-05-07 19:10:29 +0800

    2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = required('express')
-const serveStatic = required('serve-static')
-const path = required('path')
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
 const app = express()
 
@@ -10,6 +10,6 @@ app.get(/.*/, function(req,res){
     res.sendFile(path.join(_dirname,'/dist/index.html'))
 })
 
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
